60b3e746954ab9b3ae126c37a913b5f6a860d49f
Void7 <limen191100@gmail.com>
2021-10-30 19:48:55 +0300

    Add character
    Add character's sprites in folder characters.
    Draw knight sprite in the middle of the screen.

--- main.cpp
+++ main.cpp
@@ -3,10 +3,19 @@
 
 struct TextureData
 {
-    Texture2D texure {};
+    Texture2D texture {};
     Vector2 pos {0.0f, 0.0f};
     float rotation {0.0f};
     float scale {1.0f};
+};
+
+struct AnimData
+{
+    Rectangle source {0.0f, 0.0f, 0.0f, 0.0f};
+    Rectangle dest {0.0f, 0.0f, 0.0f, 0.0f};
+    const Vector2 origin {0.0f, 0.0f};
+    int frame {0};
+    int const maxFrame {5};
 };
 
 int main ()
@@ -19,12 +28,24 @@ int main ()
     // Background map
     Texture2D map = LoadTexture("nature_tileset/1Level.png");
     TextureData myBG;
-    myBG.texure = map;
+    myBG.texture = map;
     myBG.scale = 4.0f;
 
-    // Hero params
+    // Knight params
+    Texture2D knight = LoadTexture("characters/knight_idle_spritesheet.png");
     Vector2 direction {0.0f, 0.0f};
     const float speed {10};
+    TextureData knightText;
+    knightText.texture = knight;
+    knightText.scale = 4.0f;
+    AnimData knightAnim;
+    knightAnim.source.width = knight.width / (knightAnim.maxFrame + 1);
+    knightAnim.source.height = knight.height;
+    knightAnim.source.x = knightAnim.source.width * knightAnim.frame;
+    knightAnim.dest.width = knightAnim.source.width * knightText.scale;
+    knightAnim.dest.height = knightAnim.source.height * knightText.scale;
+    knightAnim.dest.x = windowRes[0] / 2 - knightAnim.dest.width / 2;
+    knightAnim.dest.y = windowRes[1] / 2 - knightAnim.dest.height / 2;
 
     SetTargetFPS(60);
     while (!WindowShouldClose())
@@ -45,9 +66,11 @@ int main ()
 
         BeginDrawing();
         ClearBackground(WHITE);
-        DrawTextureEx(myBG.texure, myBG.pos, myBG.rotation, myBG.scale, WHITE);
+        DrawTextureEx(myBG.texture, myBG.pos, myBG.rotation, myBG.scale, WHITE);
+        DrawTexturePro(knightText.texture, knightAnim.source, knightAnim.dest, knightAnim.origin, knightText.rotation, WHITE);
         EndDrawing();
     }
     UnloadTexture(map);
+    UnloadTexture(knight);
     CloseWindow();
 }
