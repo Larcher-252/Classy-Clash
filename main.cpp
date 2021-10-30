cee95f225b3e2d66d4a00124c11cdabb301454c9
Void7 <limen191100@gmail.com>
2021-10-30 19:04:23 +0300

    Add movement input
    Now when we press WASD screen moves on same direction.

--- main.cpp
+++ main.cpp
@@ -1,4 +1,5 @@
 #include "raylib.h"
+#include "raymath.h"
 
 struct TextureData
 {
@@ -21,9 +22,27 @@ int main ()
     myBG.texure = map;
     myBG.scale = 4.0f;
 
+    // Hero params
+    Vector2 direction {0.0f, 0.0f};
+    const float speed {10};
+
     SetTargetFPS(60);
     while (!WindowShouldClose())
     {
+        direction.x = 0;
+        direction.y = 0;
+        if (IsKeyDown(KEY_A)) direction.x -= 1;
+        if (IsKeyDown(KEY_D)) direction.x += 1;
+        if (IsKeyDown(KEY_W)) direction.y -= 1;
+        if (IsKeyDown(KEY_S)) direction.y += 1;
+
+        if (Vector2Length(direction) != 0.0f)
+        {
+            direction = Vector2Normalize(direction);
+            direction = Vector2Scale(direction, speed);
+            myBG.pos = Vector2Subtract(myBG.pos, direction);
+        }
+
         BeginDrawing();
         ClearBackground(WHITE);
         DrawTextureEx(myBG.texure, myBG.pos, myBG.rotation, myBG.scale, WHITE);
